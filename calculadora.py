68b2edaae8613e5cdcdd568362e4ce1ba6a61ee8
lucas <alvesrodrigueslucas72@gmail.com>
2026-03-10 10:23:51 -0300

    calculadora.soma

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+print("========== calculadora ==========")
+
+operaçoes = input("escolha uma operaçao:") 
+
+num1 = float(input("Digite o primeiro numero:"))
+num2 = float(input("Digite o segundo numero:"))
+
+if operaçoes == "SOMA":
+    print(num1 + num2)
